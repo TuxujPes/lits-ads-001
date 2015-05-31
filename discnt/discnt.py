759917289d406896bbfb096b91c500fbd121159d
Taras Kharuk <tuxuj.pes@yandex.ru>
2015-05-31 13:59:15 +0300

    add discnt homework solution, clean movrat folder

---
+++ discnt/discnt.py
@@ -0,0 +1,27 @@
+import os
+from quicksort import QuickSortAlgorithm
+
+input_file = 'discnt.in'
+output_file = 'discnt.out'
+disc_nth = 3
+qsort = QuickSortAlgorithm()
+# prepare data
+file_data = [line.strip() for line in open(input_file)]
+item_prices = [int(x) for x in file_data[0].split()]
+discount = int(file_data[1]) * 0.01
+
+items_q = len(item_prices)
+disc_pivot = items_q - items_q / disc_nth
+price_sum = 0
+
+# sort so we discount only most expensive ones
+qsort.sort(item_prices)
+
+# sum based on nth and discount
+for idx, price in enumerate(item_prices):
+    price_sum += price if idx < disc_pivot else price * (1 - discount)
+
+# write to file
+os.remove(output_file) # not sure how to overwrite instead of appending
+with open(output_file, 'a') as f:
+    f.write('%.2f' % price_sum)
