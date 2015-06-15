e93618b48537771a805f130d8e8a73a0c3a860c1
Taras Kharuk <tuxuj.pes@yandex.ru>
2015-06-15 19:33:00 +0300

    [draft] save almost finished solution

--- lngpok/lngpok.py
+++ lngpok/lngpok.py
@@ -2,30 +2,47 @@ from quicksort import QuickSortAlgorithm
 
 qsort = QuickSortAlgorithm()
 
+
 def main():
-    input_file_path = "testdata/lngpok.in"
+    input_file_path = "testdata/lngpok2.in"
     output_file_path = "testdata/lngpok.out"
 
     cards = read_cards_from_file(input_file_path)
-    qsort.sort(cards)
-    jokers = get_jokers(cards)
+    qsort.sort(cards)  # O(n log n)
+    jokers = get_jokers(cards)  # O( zeros n )
 
-    get_longest_streak(cards, jokers)
-    write_solution_to_file(output_file_path, jokers)
-    print cards
-    print jokers
+    uniques = get_unique_list(cards)  # O( n )
+    longest_streak = get_longest_streak(uniques, jokers) + jokers  # O(n^2)
+    write_solution_to_file(output_file_path, longest_streak)
+    print longest_streak
+
 
 def get_longest_streak(cards, jokers):
     n = len(cards)
-    for i in xrange(jokers, n):
-        for j in xrange(jokers+1, n):
-            print cards[j] - cards[i]
+    start = 1 if jokers > 0 else 0
+    longest_streak = 1
+    for i in xrange(start, n):
+        for j in xrange(i + 1, n):
+            if is_enough_jokers_to_fill(cards, i, j, jokers):
+                streak = j - i + 1
+                if streak > longest_streak:
+                    longest_streak = streak
+            else:
+                break
+    return longest_streak
 
-def get_needed_jokers_to_fill(cards, start, end, jokers):
-    for
+
+def is_enough_jokers_to_fill(cards, start, end, jokers):
+    return (cards[end] - cards[start]) - (end - start) <= jokers
+
 
 def get_unique_list(cards):
-    unique = [for card in cards]
+    uniques = []
+    for card in cards:
+        if card not in uniques:
+            uniques.append(card)
+    return uniques
+
 
 def get_jokers(cards):
     count = 0
@@ -36,13 +53,16 @@ def get_jokers(cards):
             count += 1
     return count
 
+
 def read_cards_from_file(filename):
     with open(filename, "r") as input_file:
         return [int(x) for x in input_file.readline().split()]
 
+
 def write_solution_to_file(filename, solution):
     with open(filename, 'w') as output_file:
         output_file.write(str(solution))
+
 
 if __name__ == "__main__":
     main()
