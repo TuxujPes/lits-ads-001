c7e797e653b6e059c7f67f12c14286c17673731f
Taras Kharuk <tuxuj.pes@yandex.ru>
2015-06-15 21:13:41 +0300

    fixed file pathes, remove unnecessary newlines

--- lngpok/lngpok.py
+++ lngpok/lngpok.py
@@ -2,20 +2,18 @@ from quicksort import QuickSortAlgorithm
 
 qsort = QuickSortAlgorithm()
 
-
 def main():
-    input_file_path = "testdata/lngpok2.in"
-    output_file_path = "testdata/lngpok.out"
+    input_file_path = "lngpok.in"
+    output_file_path = "lngpok.out"
 
     cards = read_cards_from_file(input_file_path)
     qsort.sort(cards)  # O(n log n)
-    jokers = get_jokers(cards)  # O( zeros n )
+    jokers = get_jokers(cards)  # O(zeros n)
 
-    uniques = get_unique_list(cards)  # O( n )
+    uniques = get_unique_list(cards)  # O(n)
     longest_streak = get_longest_streak(uniques, jokers) + jokers  # O(n^2)
     write_solution_to_file(output_file_path, longest_streak)
     print longest_streak
-
 
 def get_longest_streak(cards, jokers):
     n = len(cards)
@@ -31,10 +29,8 @@ def get_longest_streak(cards, jokers):
                 break
     return longest_streak
 
-
 def is_enough_jokers_to_fill(cards, start, end, jokers):
     return (cards[end] - cards[start]) - (end - start) <= jokers
-
 
 def get_unique_list(cards):
     uniques = []
@@ -53,16 +49,13 @@ def get_jokers(cards):
             count += 1
     return count
 
-
 def read_cards_from_file(filename):
     with open(filename, "r") as input_file:
         return [int(x) for x in input_file.readline().split()]
 
-
 def write_solution_to_file(filename, solution):
     with open(filename, 'w') as output_file:
         output_file.write(str(solution))
-
 
 if __name__ == "__main__":
     main()
